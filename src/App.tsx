f71221be359a8dc9a7384d7e80f811c82cc0ac2b
Vitalik-Litvinchuk <61794076+Vitalik-Litvinchuk@users.noreply.github.com>
2024-09-11 22:19:43 +0300

    tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import React, { Suspense } from 'react';
 import { Navigate } from 'react-router-dom';
 import { Routes, Route } from 'react-router';
 import './App.css';
 import Layout from './components/layout';
 import rootPath, { routes } from './routes';
-
-const LazyPage = React.lazy(() => import('./components/chapters/chapter-4'));
 
 const App = () => {
   return (
@@ -30,10 +27,6 @@ const App = () => {
               />
           )
         }
-        <Route
-          key={"chapter-4"}
-          path={"test"}
-          element={<Suspense><LazyPage /></Suspense>} />
       </Route>
       <Route path='*' element={<Navigate to={rootPath} />} />
     </Routes>
