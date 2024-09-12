64d591bf7711474e7a0556a7f37ee4eaf84b5f90
Vitalik-Litvinchuk <61794076+Vitalik-Litvinchuk@users.noreply.github.com>
2024-09-12 20:39:04 +0300

    fix: error

--- src/components/chapters/chapter-2/index.tsx
+++ src/components/chapters/chapter-2/index.tsx
@@ -1,10 +1,6 @@
-import { useTypedSelector } from "../../../hooks/useTypedSelector";
-
 const ChapterTwo = () => {
-    const { test } = useTypedSelector(state => state.chapterOne)
-
     return (
-        <div>Chapter 2: UI Components Libraries {test}</div>
+        <div>Chapter 2: UI Components Libraries</div>
     )
 }
 
